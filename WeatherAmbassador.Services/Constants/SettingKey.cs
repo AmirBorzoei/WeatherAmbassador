a3579cc15be20c99d570eeee37c65d69986b563f
Amir <Borzoei.A@orchidpharmed.com>
2022-02-17 15:54:23 +0330

    Bugfix in Setting Key - Property Order

--- WeatherAmbassador.Services/Constants/SettingKey.cs
+++ WeatherAmbassador.Services/Constants/SettingKey.cs
@@ -4,9 +4,9 @@ namespace WeatherAmbassador.Services.Constants
 {
     public class SettingKey
     {
-        public static List<string> AllSettingKeys = new() { WeatherApiUrl, WeatherApiCallIntervalFormat };
-        
         public static string WeatherApiUrl = "WeatherApiUrl";
         public static string WeatherApiCallIntervalFormat = "WeatherApiCallIntervalFormat";
+
+        public static List<string> AllSettingKeys = new() { WeatherApiUrl, WeatherApiCallIntervalFormat };
     }
 }
